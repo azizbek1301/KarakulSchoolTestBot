86c5b36bafdb403fa135a058d5d90530ccca4789
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-02 21:27:33 +0500

    User model created

---
+++ src/CheckTestBot.Domain/Entites/User.cs
@@ -0,0 +1,10 @@
+﻿namespace CheckTestBot.Domain.Entites
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname {  get; set; }
+        public string Contact {  get; set; }
+    }
+}
