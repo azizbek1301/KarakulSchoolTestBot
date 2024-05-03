a53d80d3162eb57c20d78003c88400f5336aea2c
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-03 20:20:47 +0500

    Services

---
+++ src/CheckTestBot.Domain/Services/ConfigureWebHook.cs
@@ -0,0 +1,60 @@
+﻿using CheckTestBot.Domain.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace CheckTestBot.Domain.Services
+{
+    public class ConfigureWebHook : IHostedService
+    {
+        private readonly ILogger<ConfigureWebHook> _logger;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly BotConfiguration? _botConfig;
+
+        public ConfigureWebHook(ILogger<ConfigureWebHook> logger, 
+            IServiceProvider serviceProvider,
+            IConfiguration configuration)
+        {
+            _logger= logger;
+            _serviceProvider = serviceProvider;
+            _botConfig = configuration.GetSection("BotConfiguration").Get<BotConfiguration>();
+            
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _serviceProvider.CreateScope();
+
+            var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
+
+
+            var webhookAddress = $@"{_botConfig.HostAddress}/bot/{_botConfig.Token}";
+            _logger.LogInformation("Setting webhook");
+
+            await botClient.SendTextMessageAsync(
+               chatId: 1455580577,
+               text: "Webhook o'rnatilmoqda");
+
+            await botClient.SetWebhookAsync(
+                url: webhookAddress,
+                allowedUpdates: Array.Empty<UpdateType>(),
+                cancellationToken: cancellationToken);
+
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _serviceProvider.CreateScope();
+
+            var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
+
+            _logger.LogInformation("Webhook removing");
+
+            await botClient.SendTextMessageAsync(
+                chatId: 1455580577,
+                text: "Bot Uxlamoqda");
+        }
+    }
+}
---
+++ src/CheckTestBot.Domain/Services/HandleUpdateService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+
+namespace CheckTestBot.Domain.Services
+{
+    public class HandleUpdateService
+    {
+        private readonly ILogger<ConfigureWebHook> _logger;
+        private readonly ITelegramBotClient _botClient;
+
+        public HandleUpdateService(ILogger<ConfigureWebHook> logger,ITelegramBotClient botClient )
+        {
+            _logger=logger;
+            _botClient=botClient;
+        }
+    }
+}
