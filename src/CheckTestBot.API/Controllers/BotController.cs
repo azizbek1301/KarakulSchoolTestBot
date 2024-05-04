900ad12eebaac3af79e7012c5d5587a92e0c72b5
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-04 13:36:47 +0500

    Controller Added

---
+++ src/CheckTestBot.API/Controllers/BotController.cs
@@ -0,0 +1,25 @@
+﻿using CheckTestBot.Domain.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+
+namespace CheckTestBot.API.Controllers
+{
+    public class BotController : ControllerBase
+    {
+        private readonly HandleUpdateService _handleUpdateService;
+
+        public BotController(HandleUpdateService handleUpdateService)
+        {
+            _handleUpdateService = handleUpdateService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Update update)
+        {
+            await _handleUpdateService.HandleUpdateAsync(update);
+            return Ok();
+        }
+
+    }
+}
