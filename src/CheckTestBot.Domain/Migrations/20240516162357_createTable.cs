2d2dd5f1e8c8ce6a57e1b932b2363e25c365b2b0
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-16 21:34:49 +0500

    Add migration

---
+++ src/CheckTestBot.Domain/Migrations/20240516162357_createTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CheckTestBot.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class createTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IsBot = table.Column<bool>(type: "boolean", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    Username = table.Column<string>(type: "text", nullable: true),
+                    LanguageCode = table.Column<string>(type: "text", nullable: true),
+                    IsPremium = table.Column<bool>(type: "boolean", nullable: true),
+                    AddedToAttachmentMenu = table.Column<bool>(type: "boolean", nullable: true),
+                    CanJoinGroups = table.Column<bool>(type: "boolean", nullable: true),
+                    CanReadAllGroupMessages = table.Column<bool>(type: "boolean", nullable: true),
+                    SupportsInlineQueries = table.Column<bool>(type: "boolean", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ src/CheckTestBot.Domain/Migrations/20240516163246_createTable1.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckTestBot.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class createTable1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddedToAttachmentMenu",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CanJoinGroups",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CanReadAllGroupMessages",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsBot",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsPremium",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SupportsInlineQueries",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Users",
+                newName: "Surname");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Contact",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Contact",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Surname",
+                table: "Users",
+                newName: "FirstName");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AddedToAttachmentMenu",
+                table: "Users",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CanJoinGroups",
+                table: "Users",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CanReadAllGroupMessages",
+                table: "Users",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBot",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPremium",
+                table: "Users",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LanguageCode",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SupportsInlineQueries",
+                table: "Users",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
