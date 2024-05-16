dd23db2f68cfae648e66fa4fa9ff26b26fd5dd45
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-16 21:35:07 +0500

    Connect DataBase

---
+++ src/CheckTestBot.Domain/Data/BotDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CheckTestBot.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CheckTestBot.Domain.Data
+{
+    public class BotDbContext : DbContext
+    {
+
+        public BotDbContext(DbContextOptions<BotDbContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            Database.Migrate();
+
+        }
+
+        public virtual DbSet <Users> Users { get; set; }
+
+
+    }
+}
