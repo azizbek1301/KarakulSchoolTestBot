1a46f6deba048364872c79253f42af0a8c4b9714
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-17 18:34:35 +0500

    Sertificate Table Created

---
+++ src/CheckTestBot.Domain/Entites/Sertificate.cs
@@ -0,0 +1,12 @@
+﻿namespace CheckTestBot.Domain.Entites
+{
+    public class Sertificate
+    {
+        public long Id { get; set; }
+        public string FilePath { get; set; }
+
+        public long UserId { get; set; }
+
+        public Users User { get; set; }
+    }
+}
