b82def4bfbbff28af54c4d0e2291d5eb86c1de33
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-18 20:24:54 +0500

    IApplicationDbContext

---
+++ src/CheckTestBot.Domain/Application/Abstruction/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CheckTestBot.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckTestBot.Domain.Application.Abstruction
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Subjects> Subjects { get; set; }
+        public DbSet<TestQuestions> TestQuestions { get; set; }
+        public DbSet<Sertificate> Sertificates { get; set; }
+
+        public ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
