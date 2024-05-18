fe1fb5d47ddc1df86f2c1a20525190dd7f400421
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-18 20:26:52 +0500

    test question model

--- src/CheckTestBot.Domain/Entites/TestQuestions.cs
+++ src/CheckTestBot.Domain/Entites/TestQuestions.cs
@@ -1,6 +1,8 @@
-﻿namespace CheckTestBot.Domain.Entites
+﻿using CheckTestBot.Domain.Common;
+
+namespace CheckTestBot.Domain.Entites
 {
-    public class TestQuestions
+    public class TestQuestions:Auiditable
     {
         public long Id { get; set; }
         public string TestKeys {  get; set; }
