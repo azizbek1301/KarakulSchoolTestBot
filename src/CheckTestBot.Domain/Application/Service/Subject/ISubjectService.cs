143a038c8adeb0bd35594e8a77953e4953968f22
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-18 20:25:12 +0500

    Subject Service

---
+++ src/CheckTestBot.Domain/Application/Service/Subject/ISubjectService.cs
@@ -0,0 +1,12 @@
+﻿using CheckTestBot.Domain.Entites;
+
+namespace CheckTestBot.Domain.Application.Service.Subject
+{
+    public interface ISubjectService
+    {
+        ValueTask<Subjects> AddAsync(Subjects subject);
+        ValueTask<Subjects> UpdateAsync(Subjects subject,int id);
+        ValueTask<bool> DeleteAsync(int id);
+        ValueTask<Subjects> GetSubjectAsync(int id);
+    }
+}
---
+++ src/CheckTestBot.Domain/Application/Service/Subject/SubjectService.cs
@@ -0,0 +1,68 @@
+﻿using CheckTestBot.Domain.Application.Abstruction;
+using CheckTestBot.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckTestBot.Domain.Application.Service.Subject
+{
+    public class SubjectService : ISubjectService
+    {
+        private readonly IApplicationDbContext _context;
+
+        public SubjectService(IApplicationDbContext context)
+        
+           => _context = context;       
+            
+        
+        public async ValueTask<Subjects> AddAsync(Subjects subject)
+        {
+            var storageSubject = await _context.Subjects.FirstOrDefaultAsync(x => x.Id == subject.Id);
+            if(storageSubject == null)
+            {
+                var entry = await _context.Subjects.AddAsync(subject);
+                await _context.SaveChangesAsync();
+                return entry.Entity;
+            }
+            else
+            {
+                return storageSubject;
+            }
+        }
+
+        public async ValueTask<bool> DeleteAsync(int id)
+        {
+            var storageSubject = await _context.Subjects.FirstOrDefaultAsync(x => x.Id == id);
+            if(storageSubject == null)
+            {
+                return false;
+            }
+            else
+            {
+                _context.Subjects.Remove(storageSubject);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+        }
+
+        public async ValueTask<Subjects> GetSubjectAsync(int id)
+        {
+            var storageSubject = await _context.Subjects.FirstOrDefaultAsync(x => x.Id == id);
+            return storageSubject;
+        }
+
+        public async ValueTask<Subjects> UpdateAsync(Subjects subject, int id)
+        {
+            var storageSubject = await _context.Subjects.FirstOrDefaultAsync(x => x.Id == id);
+            if(storageSubject == null)
+            {
+                return null;
+            }
+            else
+            {
+                storageSubject.SubjectName = subject.SubjectName;
+                var entry=_context.Subjects.Update(storageSubject);
+                await _context.SaveChangesAsync();
+                return entry.Entity;
+            }
+        }
+    }
+}
