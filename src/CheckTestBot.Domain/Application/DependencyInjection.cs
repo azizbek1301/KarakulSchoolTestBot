758f9b9e3bee3453b4cc517accdc3b8e1db5c6b2
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-18 20:27:35 +0500

    injection

---
+++ src/CheckTestBot.Domain/Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using CheckTestBot.Domain.Application.Service.Subject;
+using CheckTestBot.Domain.Application.Service.User;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CheckTestBot.Domain.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<ISubjectService, SubjectService>();
+            services.AddScoped<IUserService, UserService>();
+
+
+            return services;
+
+        }
+    }
+}
