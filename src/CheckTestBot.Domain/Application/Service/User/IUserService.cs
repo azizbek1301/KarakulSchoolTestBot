be5faacc3062104af77674e73c847a0cdbd2297d
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-18 20:25:31 +0500

    User service

---
+++ src/CheckTestBot.Domain/Application/Service/User/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using CheckTestBot.Domain.Entites;
+
+namespace CheckTestBot.Domain.Application.Service.User
+{
+    public interface IUserService
+    {
+        public ValueTask<Users> AddAsync(Users user);
+        public ValueTask<Users> UpdateUserPhoneNumberAsync(long userId, string phoneNumber);
+        public ValueTask<Users> GetUserAsync(long userId);  
+        public ValueTask<bool> DeleteAsync(long userId);
+    }
+}
---
+++ src/CheckTestBot.Domain/Application/Service/User/UserService.cs
@@ -0,0 +1,64 @@
+﻿using CheckTestBot.Domain.Application.Abstruction;
+using CheckTestBot.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckTestBot.Domain.Application.Service.User
+{
+    public class UserService : IUserService
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UserService(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async ValueTask<Users> AddAsync(Users user)
+        {
+            var res=await _context.Users.FirstOrDefaultAsync(x=>x.Id == user.Id);
+            if(res == null)
+            {
+                var entery= await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+                return entery.Entity;
+            }
+            return res;
+            
+        }
+
+        public async ValueTask<bool> DeleteAsync(long userId)
+        {
+            var res = await _context.Users.FirstOrDefaultAsync(x=>x.Id==userId);
+            if(res == null)
+            {
+                return false;
+            }
+            _context.Users.Remove(res);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async ValueTask<Users> GetUserAsync(long userId)
+        {
+            var res= await _context.Users.FirstOrDefaultAsync(x=>x.Id== userId);
+            return res;
+        }
+
+        public async ValueTask<Users> UpdateUserPhoneNumberAsync(long userId, string phoneNumber)
+        {
+            var res = await _context.Users.FirstOrDefaultAsync(x => x.Id == userId);
+            if( res == null)
+            {
+                return res;
+            }
+            else
+            {
+                res.Contact = phoneNumber;
+
+                var entery=_context.Users.Update(res);
+                await _context.SaveChangesAsync();
+                return entery.Entity;
+            }
+        }
+    }
+}
